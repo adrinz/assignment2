ddf9c52061e276b43ab22b87cbac173876166941
tech <tech.united85@gmail.com>
2019-11-16 14:04:53 -0500

    added new thing

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -19,6 +19,8 @@ namespace Assignment2
 
 
             //User Input
+            //new thing
+     
             Console.WriteLine("Consumed Units: ");
             ConsumedUnits = double.Parse(Console.ReadLine());
 
